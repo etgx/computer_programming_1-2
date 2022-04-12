bc2a7664ab3112d32f28cbd1426d2d532f48f3bd
FrankCCCCC <unaxultraspaceos5@gmail.com>
2022-04-12 12:22:11 +0800

    working on 13441

--- I2P_II_Kuo/mid1_practice/13441/13441.cpp
+++ I2P_II_Kuo/mid1_practice/13441/13441.cpp
@@ -7,6 +7,10 @@
 // #include <utility>
 // #include <functional>
 
+#define NOT_TRAVERSED 0
+#define TRAVERSING 1
+#define TRAVERSED 2
+
 class Edge{
     public:
     int dest = 0, weight = 0;
@@ -21,18 +25,20 @@ class Node{
     int x = 0, dia0 = 0, dia1 = 0, dis0 = 0, dis1 = 0, is_traversed = 0;
     std::vector<Edge> adj = std::vector<Edge>();
     Node(){
-        this->x = this->dia0 = this->dia1 = this->dis0 = this->dis1 = this->is_traversed = 0;
+        this->x = this->dia0 = this->dia1 = this->dis0 = this->dis1 = 0;
+        this->is_traversed = NOT_TRAVERSED;
         this->adj = std::vector<Edge>();
     }
     Node(int x){
         this->x = x;
         this->dia0 = this->dia1 = this->dis0 = this->dis1 = this->is_traversed = 0;
+        this->is_traversed = NOT_TRAVERSED;
         this->adj = std::vector<Edge>();
     }
 };
 
 bool comp_dis(std::pair<int, int> a, std::pair<int, int> b){
-    return a.second < b.second;
+    return a.second > b.second;
 }
 
 class Tree{
@@ -45,7 +51,7 @@ class Tree{
         this->n = n;
         this->map = new Node[this->n];
         for(int i = 0; i < this->n; i++){
-            this->map[i].x = 0;
+            this->map[i].x = i;
         }
     }
 
@@ -64,52 +70,55 @@ class Tree{
         }
     }
 
-    int build(Node *root){
-        if(root != NULL) return 0;
+    std::pair<int, int> build(Node *root){
+        if(root == NULL) return std::make_pair(0, 0);
 
-        if(root->adj.size() == 0){
+        root->is_traversed = TRAVERSING;
+        if(root->adj.size() <= 0){
+            // Single node
+        }else if(root->adj.size() <= 1 && this->map[root->adj.at(0).dest].is_traversed != NOT_TRAVERSED){
             // Leaf
-            
         }else{
             for(std::vector<Edge>::iterator it = root->adj.begin(); it != root->adj.end(); it++){
                 Node &node = this->map[(*it).dest];
-                if(!node.is_traversed){
+                if(node.is_traversed == NOT_TRAVERSED){
+                    printf("Traveling [%d]\n", node.x);
                     this->build(&node);
                 }
             }
 
             // Sort Distance
-            std::vector<std::pair<int, int>> dis_v;
+            printf("[%d]: ", root->x);
+            std::vector<std::pair<int, int>> dis_v(2, std::make_pair(-1, 0));
             for(std::vector<Edge>::iterator it = root->adj.begin(); it != root->adj.end(); it++){
+                int w_it = (*it).weight;
                 Node &node = this->map[(*it).dest];
-                if(node.is_traversed){
-                    dis_v.push_back(std::make_pair(node.x, node.dis0));
+                if(node.is_traversed == TRAVERSED){
+                    dis_v.push_back(std::make_pair(node.x, node.dis0 + w_it));
+                    dis_v.push_back(std::make_pair(node.x, node.dis1 + w_it));
+                    printf(" %d(%d, %d)", node.x, node.dis0 + w_it, node.dis1 + w_it);
                 }
             }
 
             std::sort(dis_v.begin(), dis_v.end(), comp_dis);
             root->dis0 = dis_v.at(0).second;
             root->dis1 = dis_v.at(1).second;
-
-            // Sort Distance
-            std::vector<std::pair<int, int>> dis_v;
-            for(std::vector<Edge>::iterator it = root->adj.begin(); it != root->adj.end(); it++){
-                Node &node = this->map[(*it).dest];
-                if(node.is_traversed){
-                    dis_v.push_back(std::make_pair(node.x, node.dis0));
-                }
-            }
-
-            std::sort(dis_v.begin(), dis_v.end(), comp_dis);
-            root->dis0 = dis_v.at(0).second;
-            root->dis1 = dis_v.at(1).second;
+            int dis0_node = dis_v.at(0).first, dis1_node = dis_v.at(1).first;
+            printf(" -> DIS_0: %d(%d) | DIS_1: %d(%d)\n", dis0_node, root->dis0, dis1_node, root->dis1);
 
             // Sort Diameter
-            std::vector<std::pair<int, int>> dia_v;
+            std::vector<std::pair<int, int>> dia_v(2, std::make_pair(-1, 0));
             for(std::vector<Edge>::iterator it = root->adj.begin(); it != root->adj.end(); it++){
+                int w_it = (*it).weight;
                 Node &node = this->map[(*it).dest];
-                if(node.is_traversed){
+                if(node.is_traversed == TRAVERSED){
                     dia_v.push_back(std::make_pair(node.x, node.dia0));
+                    if(node.x == dis0_node){
+                        dia_v.push_back(std::make_pair(node.x, node.dis0 + w_it + root->dis1));
+                    }else{
+                        dia_v.push_back(std::make_pair(node.x, node.dis0 + w_it + root->dis0));
+                    }
+                    dia_v.push_back(std::make_pair(node.x, node.dia1));
                 }
             }
 
@@ -117,11 +126,13 @@ class Tree{
             root->dia0 = dia_v.at(0).second;
             root->dia1 = dia_v.at(1).second;
         }
-        root->is_traversed = 1;
+        root->is_traversed = TRAVERSED;
+        return std::make_pair(root->dia0, root->dia1);
     }
 
     int solve(){
-
+        std::pair<int, int> dias = this->build(&this->map[0]);
+        return dias.second;
     }
 };
 
@@ -135,6 +146,7 @@ int main(){
         // printf("%d %d %d\n", u_i, v_i, w_i);
         tree.add_edge(u_i, v_i, w_i);
     }
+    printf("%d\n", tree.solve());
     tree.show();
     return 0;
 }
